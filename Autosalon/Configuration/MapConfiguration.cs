3d3e9090b8ab9216f94c96e613e075dbc8948d9b
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-12-24 20:20:37 +0500

    Configuration

---
+++ Autosalon/Configuration/MapConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Autosalon.Domain.Entities.Cars;
+using Autosalon.Domain.Entities.Users;
+using Autosalon.Service.Dto_s;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Autosalon.Configuration
+{
+    public class MapConfiguration : Profile
+    {
+        public MapConfiguration()
+        {
+            CreateMap<Car, CarDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
